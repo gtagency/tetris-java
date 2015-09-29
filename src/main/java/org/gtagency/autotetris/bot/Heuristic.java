eae3013d2734be5d436596e1ab43935e4c2947d9
wwhitlow <wwhitlow@gatech.edu>
2015-09-29 19:08:08 -0400

    Revert "Added heuristic and utility interfaces"
    This reverts commit 66559bc828be6f96cd39749a2986d903f20d46eb.

--- src/main/java/org/gtagency/autotetris/bot/Heuristic.java
+++
@@ -1,7 +0,0 @@
-package org.gtagency.autotetris.bot;
-
-import org.gtagency.autotetris.field.Field;
-
-public interface Heuristic {
-    double value(Field f);
-}
--- src/main/java/org/gtagency/autotetris/bot/Utility.java
+++
@@ -1,7 +0,0 @@
-package org.gtagency.autotetris.bot;
-
-import org.gtagency.autotetris.field.Field;
-
-public interface Utility {
-    double value(Field f);
-}
