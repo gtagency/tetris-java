0766fc5010273a1a3216fe6cd7f2c1f533f027f4
Cole Taylor <coletaylor788@gmail.com>
2015-09-29 19:23:49 -0400

    Added a value method to determine the number of completed rows

---
+++ src/main/java/org/gtagency/autotetris/bot/ColeUtility.java
@@ -0,0 +1,29 @@
+package org.gtagency.autotetris.bot;
+
+import org.gtagency.autotetris.field.Field;
+import org.gtagency.autotetris.bot.Utility;
+
+public class ColeUtility implements Utility {
+
+    // Return the number of completed rows
+    public double value(Field f) {
+        int completedRows = 0;
+        for (int i = 0; i < f.getHeight(); i++) {
+            boolean complete = true;
+            boolean isEmpty = true;
+            for (int j = 0; j < f.getWidth(); j++) {
+                if (f.getCell(i, j).isBlock()) {
+                    isEmpty = false;
+                } else {
+                    complete = false;
+                }
+            }
+            if (complete) {
+                completedRows++;
+            } else if (!isEmpty) {
+                break;
+            }
+        }
+        return completedRows;
+    }
+}
